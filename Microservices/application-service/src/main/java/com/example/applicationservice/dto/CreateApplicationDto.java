dc364369ca5a625b997820371df29ad20fdf3ad2
NHT2610 <nguyenhuutruc26102002@gmail.com>
2024-06-09 10:46:41 +0700

    application-service basic api

---
+++ Microservices/application-service/src/main/java/com/example/applicationservice/dto/CreateApplicationDto.java
@@ -0,0 +1,10 @@
+package com.example.applicationservice.dto;
+
+public class CreateApplicationDto {
+  public String employerId;
+  public String jobId;
+  public String jobSeekerId;
+  public String applicationName;
+  public String cvLink;
+  public String coverLetter;
+}
