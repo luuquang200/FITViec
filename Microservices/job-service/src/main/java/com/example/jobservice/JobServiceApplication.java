950fe7a8abbf4b9458192b0404dec8f2d79ace56
NHT2610 <nguyenhuutruc26102002@gmail.com>
2024-06-11 23:47:58 +0700

    Fix firebase SDK file path

--- Microservices/job-service/src/main/java/com/example/jobservice/JobServiceApplication.java
+++ Microservices/job-service/src/main/java/com/example/jobservice/JobServiceApplication.java
@@ -8,14 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class JobServiceApplication {
 
 	public static void main(String[] args) throws IOException {
-		FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+		//FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+		InputStream serviceAccount = JobServiceApplication.class.getClassLoader().getResourceAsStream("serviceAccountKey.json");
+		if (serviceAccount == null) {
+			throw new IllegalArgumentException("File serviceAccountKey.json not found in classpath");
+		}
 		FirebaseOptions options = new FirebaseOptions.Builder()
 				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
 				.build();
