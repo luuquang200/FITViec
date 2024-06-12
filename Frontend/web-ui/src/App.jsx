bdeb82f4dd8725b7f040ccbf242fbd22fc6d3471
ltk2801 <luutuankhanha3dt@gmail.com>
2024-06-13 00:52:49 +0700

    Add Settings Page User ( Update Password & Update Username & Delete User )

--- Frontend/web-ui/src/App.jsx
+++ Frontend/web-ui/src/App.jsx
@@ -30,6 +30,7 @@ import ProfileManagementCv from "./components/profile/profile-management-cv";
 import ProfileManagementJob from "./components/profile/profile-management-job";
 import AdminDashboard from "./components/Admin/admin-dashboard";
 import EmployeeJobManagment from "./components/Employee/employee-job-managment";
+import SettingsAccount from "./components/Authentications/settingsAccount";
 
 function App() {
     return (
@@ -40,7 +41,10 @@ function App() {
             <div className="h-[88px]" />
             <Routes>
                 <Route path="/employer" element={<EmployerContainer />} />
-                <Route path="/cv-detail/:applicationId" element={<CVViewer/>} />
+                <Route
+                    path="/cv-detail/:applicationId"
+                    element={<CVViewer />}
+                />
                 <Route path="/sign_in" element={<SignIn />} />
                 <Route path="/sign_up" element={<SignUp />} />
                 <Route
@@ -70,10 +74,7 @@ function App() {
                     path="/profile-cv/job-preferences"
                     element={<ProfileManagementJob />}
                 />
-                <Route
-                    path="/my-jobs/"
-                    element={<EmployeeJobManagment />}
-                />
+                <Route path="/my-jobs/" element={<EmployeeJobManagment />} />
                 <Route
                     path="/verify_email"
                     element={
@@ -100,6 +101,7 @@ function App() {
                     }
                 />
                 <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/settings" element={<SettingsAccount />} />
             </Routes>
             <Footer />
         </AuthProvider>
