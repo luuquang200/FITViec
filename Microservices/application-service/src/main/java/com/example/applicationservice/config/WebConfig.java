f1d99ba08521067e96578d394967601d0747517a
quangluu <luungocquang2002@gmail.com>
2024-06-13 23:47:12 +0700

    chore: Update package name in WebConfig.java

--- Microservices/application-service/src/main/java/com/example/applicationservice/config/WebConfig.java
+++ Microservices/application-service/src/main/java/com/example/applicationservice/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.config;
+package com.example.applicationservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
