75f9f49526f57ed59d508326742b4e19ec1cda60
Dat Phan <phanduongquocdat273@gmail.com>
2024-06-16 17:26:07 +0700

    divide company-detail-page into multiple components

--- Frontend/web-ui/src/components/protected-route.jsx
+++ Frontend/web-ui/src/components/protected-route.jsx
@@ -5,7 +5,7 @@
 
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/authContext";
+import { useAuth } from "@/contexts/authContext";
 
 const ProtectedRoute = ({ children, allowedRole = "none" }) => {
     const { currentUser } = useAuth();
